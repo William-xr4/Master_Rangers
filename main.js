12afbff721d123a35fb6d51b57b2148ba328220a
William-xr4 <108703320+William-xr4@users.noreply.github.com>
2023-01-12 16:21:43 -0300

    Add files via upload
    Atualizando meu site!

--- main.js
+++ main.js
@@ -1,25 +1,53 @@
-var canvas = document.getElementById("canvas");
-var contexto = canvas.getContext("2d");
-var altura_do_jogador = 180;
-var largura_do_jogador = 190;
+var imagem_objeto ="";
+var canvas = new fabric.Canvas("canvas");
+var pos_x;
+var pos_y;
 
-if(tecla=="84"){
-    atualiza_bloco("br_M.R.png");
-    console.log("a foi pressionada");
+function atualiza_imagem(imagem, largura, altura){
+    fabric.Image.fromURL(imagem, function(img){
+        imagem_objeto=img;
+        imagem_objeto.scaleToWidth(largura);
+        imagem_objeto.scaleToHeight(altura);
+        imagem_objeto.set({
+            top:pos_y, 
+            left:pos_x
+        });
+        canvas.add(imagem_objeto);
+    });
 }
-if(tecla=="69"){
-    atualiza_bloco("_M.R.png");
-    console.log("b foi pressionada");
-}
-if(tecla=="67"){
-    atualiza_bloco("_M.R.png");
-    console.log("c foi pressionada");
-}
-if(tecla=="68"){
-    atualiza_bloco("_M.R.png");
-    console.log("d foi pressionada");
-}
-if(tecla=="69"){
-    atualiza_bloco("_M.R.png");
-    console.log("e foi pressionada");
+window.addEventListener("keydown", tecla_pressionada);
+
+function tecla_pressionada(evento){
+    var tecla =evento.keyCode;
+
+    if(tecla=="69"){
+        pos_x =-60;
+        pos_y =300;
+        atualiza_imagem("br_M.R.png", 150, 250);
+        console.log("e foi pressionada");
+    }
+    if(tecla=="86"){
+        pos_x =200;
+        pos_y =250;
+        atualiza_imagem("gr.png", 200, 300);
+        console.log("v foi pressionada");
+    }
+    if(tecla=="65"){
+        pos_x =330;
+        pos_y =250;
+        atualiza_imagem("pr.png", 140, 240);
+        console.log("a foi pressionada");
+    }
+    if(tecla=="82"){
+        pos_x =440;
+        pos_y =250;
+        atualiza_imagem("rr1.png", 200, 280);
+        console.log("r foi pressionada");
+    }
+    if(tecla=="73"){
+        pos_x =600;
+        pos_y =250;
+        atualiza_imagem("yr.png", 200, 280);
+        console.log("i foi pressionada");
+    }
 }
